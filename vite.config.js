6dca09988ff286cca6dd93cc2baf5ab6fc94f676
yuchan <yuchan613@naver.com>
2025-10-24 12:44:32 +0900

    catch error in vite setting and package (in build)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',        // index.html이 있는 폴더
-  base: './', // 배포/개발 시 경로 앞에 붙음
+  base: '/', // 배포/개발 시 경로 앞에 붙음
   publicDir: 'public', // WASM 파일 등 static 파일 위치
   build: {
     outDir: 'dist',    // 빌드 결과물 폴더
