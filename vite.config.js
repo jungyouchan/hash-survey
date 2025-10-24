01c2c83ce6ac0d29f59baeee4640c5876ad8ed14
yuchan <yuchan613@naver.com>
2025-10-24 12:35:45 +0900

    change vite setting to build

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',        // index.html이 있는 폴더
-  base: '/hash-survey/', // 배포/개발 시 경로 앞에 붙음
+  base: './', // 배포/개발 시 경로 앞에 붙음
   publicDir: 'public', // WASM 파일 등 static 파일 위치
+  build: {
+    outDir: 'dist',    // 빌드 결과물 폴더
+  },
   server: {
     port: 5173,     // 개발 서버 포트
   }
